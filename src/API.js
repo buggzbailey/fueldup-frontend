c9936869315284b93bc18b34f7981254f7923ab4
Trevoree Dockery <buggzbailey@gmail.com>
2022-10-04 18:02:37 -0400

    added backend url

--- src/API.js
+++ src/API.js
@@ -10,7 +10,7 @@ var baseURL;
 // } else {
 // baseURL = "http://127.0.0.1:8000";
 // }
-baseURL = "https://backend-fueled.herokuapp.com";
+baseURL = "https://fueldup.trevoreedockery.repl.co/";
 
 const api = axios.create({
   baseURL: baseURL,
